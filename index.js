3d562bcf0828e9069b2937a15103cf06723331ad
Piyush <pshtiwari0@gmail.com>
2024-07-10 00:21:03 +0530

    first commit - login api

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from "express";
+const app = express();
+
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import commentRoutes from "./routes/comments.js";
+import likeRoutes from "./routes/likes.js";
+
+// middlewares
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/comments", commentRoutes);
+app.use("/api/likes", likeRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Express Server up");
+});
+
+app.listen(8800, () => {
+  console.log("Express serve listening on port 8800");
+});
